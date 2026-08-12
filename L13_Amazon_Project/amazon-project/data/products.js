9628c1f369bce8327dc3f332e2fa2744248c3f34
Nabeel-8090 <sheikhnabeel2201182@gmail.com>
2026-08-12 21:57:28 +0500

    convert loadProducts from XHR to fetch

--- L13_Amazon_Project/amazon-project/data/products.js
+++ L13_Amazon_Project/amazon-project/data/products.js
@@ -73,6 +73,32 @@ export class Appliance extends Product {
 
 export let products = [];
 
+export function loadProductsFetch() {
+  const promise = fetch(
+    'https://supersimplebackend.dev/products'
+  ).then((response) => {
+    return response.json();
+  }).then((productsData) => {
+    products = productsData.map((productDetails) => {
+      if (productDetails.type === 'clothing') {
+        return new Clothing(productDetails);
+      } else if (productDetails.type === 'appliance') {
+        return new Appliance(productDetails);
+      }
+      return new Product(productDetails);
+    });
+    console.log('load products');
+  });
+  return promise;
+}
+
+/*
+loadProductsFetch().then(() => {
+  console.log("Next Step");
+});
+*/
+
+/*
 export function loadProducts(fun) {
   const xhr = new XMLHttpRequest();
 
@@ -92,6 +118,7 @@ export function loadProducts(fun) {
   xhr.open('GET', 'https://supersimplebackend.dev/products');
   xhr.send();
 }
+*/
 
 /*
 export const products = [
--- L13_Amazon_Project/amazon-project/scripts/checkout.js
+++ L13_Amazon_Project/amazon-project/scripts/checkout.js
@@ -1,23 +1,18 @@
 import { renderOrderSummary } from './checkout/orderSummary.js';
 import { renderPaymentSummary } from './checkout/paymentSummary.js';
 import { renderCheckoutHeader } from './checkout/checkoutHeader.js';
-import { loadProducts } from '../data/products.js';
+import { loadProductsFetch } from '../data/products.js';
 import { loadCart } from '../data/cart.js';
 
 Promise.all([
-    new Promise((resolve) => {
-        loadProducts(() => {
-            resolve('value1');
-        });
-    }),
+    loadProductsFetch(),
     new Promise((resolve) => {
         loadCart(() => {
-            resolve('value2');
+            resolve('value');
         });
     })
 
-]).then((values) => {
-    console.log(values);
+]).then((value) => {
     renderOrderSummary();
     renderPaymentSummary();
     renderCheckoutHeader();
